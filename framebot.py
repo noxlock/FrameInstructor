ace0c8ff6a8e549634372e14fb2a23da93ced6d6
nox <admin@noxlock.dev>
2019-09-27 16:24:53 +1000

    more work on embeds, fixed strip

--- framebot.py
+++ framebot.py
@@ -46,26 +46,32 @@ async def on_message(message): # on message, runs function
 
 		elif message.content == "!help frame": #frame help menu, very big
 			embed = discord.Embed(
-			title = '!frame Help Menu',
+			title = 'Help Menu For !frame',
 			description = f"List of all syntax you can use with !frame"
 			)
 
 			embed.add_field(name="cmd {move}", value="Displays a selected move's info", inline=False)
 			embed.add_field(name="{plus/minus}OnBlock {True/False}", value="Pastes a link to all of the character's plus or minus frame on block moves.", inline=False)
-			embed.add_field(name="{plus/minus}OnHit {True/False}", value="Pastes a link to all of the character's plus or minus frame on hit moves.", inline=False)
-			embed.add_field(name="{plus/minus}OnCounter {True/False}", value="Pastes a link to all of the character's plus or minus frame on counter hit moves.", inline=False)
-			embed.add_field(name="OnBlock {min,max}", value="Pastes a link to all of the character's plus or minus frame on counter hit moves.", inline=False)
+			embed.add_field(name="{plus/minus}OnHit {True/False}", value="The same as {OnBlock} but for frames on hit.", inline=False)
+			embed.add_field(name="{plus/minus}OnCounter {True/False}", value="The same as {OnBlock} but for frames on counter hit.", inline=False)
+			embed.add_field(name="onBlock {min,max}", value="Pastes a link to all of the character's moves which frames are within the range on block.", inline=False)
+			embed.add_field(name="onHit {min,max}", value="The same as {onHit} but for frame data on hit", inline=False)
+			embed.add_field(name="onCounter {min,max}", value="The same as {onBlock} but for frama data on counter hit", inline=False)
+			embed.add_field(name="hit {h/m/l}", value="Pastes a link to all of the character's moves that are {high}, {mid}, or {low} hitting.", inline=False)
+			embed.add_field(name="firstHit {h/m/l}", value="The same as {hit} but only for the first hit of the move.", inline=False)
+			embed.add_field(name="lastHit {h/m/l}", value="The same as {firsthit} but for the last hit of the move.", inline=False)
+			embed.add_field(name="speed {min,max}", value="Pastes a link to all of the character's moves which startup frames are between {min,max}.", inline=False)
+			embed.add_field(name="crush {TJ/TC}", value="The same as {firsthit} but for the last hit of the move.", inline=False)
 			await message.channel.send(embed=embed)
 
 
 
 		elif message.content.startswith("!frame"):
-			message.content = message.content.strip('!frame')
 			message.content = message.content.split()
-			message.content[0] = message.content[0].title()
+			message.content[1] = message.content[1].title()
 
 
-			if message.content[0] not in charlist:
+			if message.content[1] not in charlist:
 				await message.channel.send("Check the spelling of your character, do !charlist to see all available characters.")
 
 
@@ -74,7 +80,7 @@ async def on_message(message): # on message, runs function
 
 
 			else: #if all 3 supplied then:
-				r = requests.get(f'https://t7api.herokuapp.com/character/{message.content[0]}?{message.content[1]}={message.content[2]}')		
+				r = requests.get(f'https://t7api.herokuapp.com/character/{message.content[1]}?{message.content[2]}={message.content[3]}')		
 				result = r.json()
 
 				#move on to error checking
@@ -88,10 +94,10 @@ async def on_message(message): # on message, runs function
 
 
 				else:
-					file = discord.File(f"./Thumbnails/{message.content[0]}.jpg")
+					file = discord.File(f"./Thumbnails/{message.content[1]}.jpg")
 					embed = discord.Embed(
 					title = 'FrameInstructor Data',
-					description = f"Frame Data for {message.content[0]}, {message.content[2]}", #frame data for character, cmd
+					description = f"Frame Data for {message.content[1]}, {message.content[3]}", #frame data for character, cmd
 					colour = discord.Color.red()
 					)
 					embed.add_field(name="Character", value=message.content[0], inline=True)
@@ -102,7 +108,7 @@ async def on_message(message): # on message, runs function
 					embed.add_field(name="On Block", value=result[0]['onBlock'], inline=True)
 					embed.add_field(name="On Hit", value=result[0]['onHit'], inline=True)
 					embed.add_field(name="CH", value=result[0]['onCounter'], inline=True)
-					embed.set_thumbnail(url=f"attachment://{message.content[0]}.jpg")
+					embed.set_thumbnail(url=f"attachment://{message.content[1]}.jpg")
 					#adds all the data to the embed object.
 					await message.channel.send(file=file, embed=embed)
 
